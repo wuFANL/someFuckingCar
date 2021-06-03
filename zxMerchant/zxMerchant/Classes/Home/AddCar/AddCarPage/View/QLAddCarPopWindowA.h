dac35d945015792ff5236489714abcf100880960
张精申 <694120752@qq.com>
2021-06-03 22:21:04 +0800

    弹窗

---
+++ zxMerchant/zxMerchant/Classes/Home/AddCar/AddCarPage/View/QLAddCarPopWindowA.h
@@ -0,0 +1,17 @@
+//
+//  QLAddCarPopWindowA.h
+//  zxMerchant
+//
+//  Created by 张精申 on 2021/6/3.
+//  Copyright © 2021 ql. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface QLAddCarPopWindowA : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ zxMerchant/zxMerchant/Classes/Home/AddCar/AddCarPage/View/QLAddCarPopWindowA.m
@@ -0,0 +1,21 @@
+//
+//  QLAddCarPopWindowA.m
+//  zxMerchant
+//
+//  Created by 张精申 on 2021/6/3.
+//  Copyright © 2021 ql. All rights reserved.
+//
+
+#import "QLAddCarPopWindowA.h"
+
+@implementation QLAddCarPopWindowA
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
