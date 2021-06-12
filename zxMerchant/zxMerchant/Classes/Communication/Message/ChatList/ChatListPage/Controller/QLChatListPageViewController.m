8db1e1d944f3a939c35a4b176ee5d260657e0a1b
developerhk <530449760@qq.com>
2021-06-12 19:00:12 +0800

    1

--- zxMerchant/zxMerchant/Classes/Communication/Message/ChatList/ChatListPage/Controller/QLChatListPageViewController.m
+++ zxMerchant/zxMerchant/Classes/Communication/Message/ChatList/ChatListPage/Controller/QLChatListPageViewController.m
@@ -234,7 +234,7 @@
         
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             NSIndexPath *indexpath = [NSIndexPath indexPathForRow:0 inSection:self.chatListArray.count-1];
-            [self.tableView scrollToRowAtIndexPath:indexpath atScrollPosition:UITableViewScrollPositionBottom animated:YES];
+            [self.tableView scrollToRowAtIndexPath:indexpath atScrollPosition:UITableViewScrollPositionBottom animated:NO];
         });
         
     } fail:^(NSError *error) {
