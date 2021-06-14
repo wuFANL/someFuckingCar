ded9454eda1bcf5971bddfc594cae21c6682297c
wfan112 <944720299@qq.com>
2021-06-14 19:53:17 +0800

    no message

--- zxMerchant/zxMerchant/Classes/CarSource/AdvancedScreening/Controller/QLAdvancedScreeningViewController.m
+++ zxMerchant/zxMerchant/Classes/CarSource/AdvancedScreening/Controller/QLAdvancedScreeningViewController.m
@@ -369,7 +369,7 @@
         cell.clickHandler = ^(id result) {
             NSDictionary *dic = result;
             NSInteger index = [dic[@"selectIndex"] integerValue];
-            NSString *obj = [key isEqualToString:@"car_type"]?dataArr[index] : StringWithFormat(@"%ld", index);
+            NSString *obj = dataArr[index];
             if (dataArr.count > index && obj) {
                 [self.conditionDic setObject:obj forKey:key];
             }
